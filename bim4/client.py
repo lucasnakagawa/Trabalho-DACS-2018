dcf6c4c9d9e332fdb51a0939aa836c458b485dc4
LUCAS RYUICHI MARTINEZ NAKAGAWA <lucasnakagawa@univille.br>
2018-10-24 20:12:10 -0300

    Deletando arquivo

--- bim4/client.py
+++
@@ -1,6 +0,0 @@
-class Client():
-    def __init__(self, _id=0, name=None, phone=None, email=None):
-        self._id = _id
-        self.name = name
-        self.phone = phone
-        self.email = email
